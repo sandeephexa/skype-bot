6dba0aef3e058bc644778b91182feff1845c278c
sandeephexa <31004610+sandeephexa@users.noreply.github.com>
2017-08-28 19:19:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-var builder = require("botbuilder");
-var restify = require("restify");
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
 var server = restify.createServer();
-const MICROSOFT_APP_ID = "d105ed4b-a5a0-4423-8fd4-61932d183b3f";
-const MICROSOFT_APP_PASSWORD = "DooeZfWb9ktfrxVwctrxPf4";
-//setting up restify server
-
-server.listen(process.env.port || process.env.PORT || 3001, function()
-{
-    console.log("%s server is listening at %s",server.name,server.url);
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
 });
 
-//create chat connector to communicate with bot framework
-
+// Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-appId : process.env.MICROSOFT_APP_ID,
-appPassword : process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
-// listen for meesages from user
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
 
-server.post('api/messages',connector.listen());
-
-// Receive messages from the user and respond by echoing 
-
-var bot =  new builder.UniversalBot(connector,function(session)
-{
-    session.send("You said %s",session.message.text);
-});   
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
+});
