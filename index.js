da1b178f35de9620a6e285ac674b493fd3fcf104
sandeephexa <31004610+sandeephexa@users.noreply.github.com>
2017-08-28 19:14:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,4 +26,4 @@ server.post('api/messages',connector.listen());
 var bot =  new builder.UniversalBot(connector,function(session)
 {
     session.send("You said %s",session.message.text);
-});
+});   
